590484030fb64dc68f0ac65286a590a733a1da26
Pricylia <pricylia884@gmail.com>
2024-06-18 19:35:19 -0300

    depoimentos

---
+++ js/site.js
@@ -0,0 +1,19 @@
+
+window.addEventListener("scroll", function(){
+    let header = this.document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 600)
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
--- site.js
+++
@@ -1,5 +0,0 @@
-
-window.addEventListener("scroll", function(){
-    let header = this.document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 600)
-})
