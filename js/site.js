b97ea71539771d82a5ce55180d8e06fee12e290a
Estagiario(a) de TI <estagiarioti@ceapepi.local>
2024-06-19 11:45:09 -0300

    casos

--- js/site.js
+++ js/site.js
@@ -8,6 +8,10 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction:false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
